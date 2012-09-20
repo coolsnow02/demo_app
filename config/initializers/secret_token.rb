3d3ee628ad581bed96014d9dad90f1bf536aaa13
nehagwebonise <webonise@webonise-desktop.(none)>
2012-09-20 14:52:20 +0530

    demo app done..

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1012a17ca8621a1319311d3c7f1b4a4bac7d22bb4f05900540a49064d52eee95939a69d3f664a7277b8bd36656ffd7820c9bc13c2c3bdf0d19a1fe5a36102061'
